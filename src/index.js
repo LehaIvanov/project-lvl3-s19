bb99f653a1f75832f22d2106592934b956dbc06f
Alex <ivanov.alexey.92@gmail.com>
2017-03-04 06:20:01 +0300

    Use temp directory

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import fs from 'mz/fs';
 import url from 'url';
 import path from 'path';
 import cheerio from 'cheerio';
+import os from 'os';
 import axios from './lib/axios';
 
 const successMsg = 'Page uploaded successfully';
+const tmpdirPrefix = 'page-loader-';
 
 const getNameByAddress = (address) => {
   const escape = str => str.replace(/\W+/g, '-');
@@ -25,10 +27,10 @@ const getResourceFileName = (src) => {
   return fileName;
 };
 
-const loadResource = async (src, mainPageUrl, downloadLocation) => {
+const loadResource = async (src, pageUrl, downloadLocation) => {
   const fileName = getResourceFileName(src);
   const urlObj = url.parse(src, true);
-  const urlForResource = urlObj.host === null ? url.resolve(mainPageUrl, src) : url.format(urlObj);
+  const urlForResource = urlObj.host === null ? url.resolve(pageUrl, src) : url.format(urlObj);
   const filePath = path.resolve(downloadLocation, fileName);
   const res = await axios.get(urlForResource);
   await fs.writeFile(filePath, res.data, 'utf8');
@@ -56,22 +58,64 @@ const getResourcesFromPage = (html) => {
     .toArray();
 };
 
-const pageLoader = async (address, downloadLocation) => {
-  const resMainPage = await axios.get(address);
-  const resources = getResourcesFromPage(resMainPage.data);
-  const dirNameForResources = `${getNameByAddress(address)}_files`;
-  const pathForResources = path.resolve(downloadLocation, dirNameForResources);
-  await fs.mkdir(pathForResources);
-  await Promise.all(resources.map(src => loadResource(src, address, pathForResources)));
+const copyFile = (source, target) => new Promise((resolve, reject) => {
+  const rs = fs.createReadStream(source);
+  rs.on('error', (err) => {
+    reject(err);
+  });
 
-  const mainPagePath = path.resolve(downloadLocation, `${getNameByAddress(address)}.html`);
-  const $ = cheerio.load(resMainPage.data);
+  const ws = fs.createWriteStream(target);
+  ws.on('error', (err) => {
+    reject(err);
+  });
+
+  rs.pipe(ws);
+
+  rs.on('end', () => {
+    resolve();
+  });
+});
+
+const getCopyResourcesPromises = (resourceTmpFiles, tmpResourcesPath, resourcesPath) =>
+  resourceTmpFiles.map(file =>
+    copyFile(path.resolve(tmpResourcesPath, file), path.resolve(resourcesPath, file)));
+
+const loadPageToTmpDir = async (address, tmpPagePath, tmpResourcesPath, dirNameForResources) => {
+  const res = await axios.get(address);
+  const html = res.data;
+  const resources = getResourcesFromPage(html);
+  await fs.mkdir(tmpResourcesPath);
+  await Promise.all(resources.map(src => loadResource(src, address, tmpResourcesPath)));
+
+  const $ = cheerio.load(html);
   $('link[href], script[src]').each(function replaceSrc() {
     const attr = getLocationAttrbute($(this));
     const src = $(this).attr(attr);
     $(this).attr(attr, path.join(dirNameForResources, getResourceFileName(src)));
   });
-  await fs.writeFile(mainPagePath, $.html(), 'utf8');
+  await fs.writeFile(tmpPagePath, $.html(), 'utf8');
+};
+
+const checkPathValid = async (inspectedPath) => {
+  await fs.stat(inspectedPath);
+};
+
+const pageLoader = async (address, downloadLocation) => {
+  await checkPathValid(downloadLocation);
+
+  const dirNameForResources = `${getNameByAddress(address)}_files`;
+  const fileNameForPage = `${getNameByAddress(address)}.html`;
+  const tmpdir = await fs.mkdtemp(path.join(os.tmpdir(), tmpdirPrefix));
+  const tmpResourcesPath = path.resolve(tmpdir, dirNameForResources);
+  const resourcesPath = path.resolve(downloadLocation, dirNameForResources);
+  const tmpPagePath = path.resolve(tmpdir, fileNameForPage);
+  const pagePath = path.resolve(downloadLocation, fileNameForPage);
+
+  await loadPageToTmpDir(address, tmpPagePath, tmpResourcesPath, dirNameForResources);
+  const resourceTmpFiles = await fs.readdir(tmpResourcesPath);
+  await fs.mkdir(resourcesPath);
+  await Promise.all([...getCopyResourcesPromises(resourceTmpFiles, tmpResourcesPath, resourcesPath),
+    copyFile(tmpPagePath, pagePath)]);
 
   return successMsg;
 };
